598567c46e699e5f428e1a95aae5c6d4885adc45
hammadkdys <hammad@kdyslab.com>
2024-01-09 13:43:17 +0500

    all

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const app = express();
+const path = require('path');
 require('dotenv').config();
 const port = process.env.PORT||3000;
 
-app.get('/', (req, res) => {
+
+app.use(express.static(path.resolve(__dirname, 'vitetest/dist')));
+
+app.get('/hammad', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'vitetest/dist', 'index.html'));
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
